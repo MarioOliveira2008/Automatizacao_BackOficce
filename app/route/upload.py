a80bd4d46e507b93fa85e260a4d54c5054607c48
MarioOliveira2008 <mario.o.jesus@aluno.senai.br>
2026-06-09 13:54:08 -0300

    Arrumando o codigo upload.py

--- app/route/upload.py
+++ app/route/upload.py
@@ -1,34 +1,15 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
-import os
+from fastapi import APIRouter, UploadFile, File
 import shutil
 
-upload = APIRouter(
-    prefix="/uploads",
-    tags=["Uploads"]
-)
-
-UPLOAD_FOLDER = "upload"
-
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
+upload = APIRouter()
 
 @upload.post("/")
 async def upload_pdf(arquivo: UploadFile = File(...)):
-
-    # Verifica se o arquivo é PDF
-    if arquivo.content_type != "application/pdf":
-        raise HTTPException(
-            status_code=400,
-            detail="Apenas arquivos PDF são permitidos."
-        )
-
-    caminho = f"{UPLOAD_FOLDER}/{arquivo.filename}"
-
-    # Salva o arquivo
-    with open(caminho, "wb") as buffer:
+    with open(f"upload/{arquivo.filename}", "wb") as buffer:
         shutil.copyfileobj(arquivo.file, buffer)
 
     return {
         "mensagem": "Upload realizado com sucesso",
         "arquivo": arquivo.filename
     }
+    
